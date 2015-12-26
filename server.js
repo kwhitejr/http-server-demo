b1dfd13de8737e252cd8cf0e0c054325a0696cfc
kwhitejr <kwhitejr.edu@gmail.com>
2015-12-26 12:27:46 -1000

    changed

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
+
 var http = require('http');
 
-var server = http.createServer(function (req, res) {
+// Creates a server. Fuction requires a request and a response. Order matters.
+var server = http.createServer(function requestHandler (req, res) {
+  // req.on('data', function (data) {
+  //   console.log(data);
+  // });
+  res.end('<h1>hello</h1>');
+});
 
+// Server needs to listen.
+server.listen(8080, function () {
+  console.log('Listening at: localhost:' + 8080);
 });
